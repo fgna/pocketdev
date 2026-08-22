d55bd64d2e1b1139eae4f333881a40adb4701df0
fgna <50234103+fgna@users.noreply.github.com>
2026-08-22 10:26:53 +0200

    Add initial unit test

---
+++ app/src/test/java/de/fgna/pocketdev/PocketDevSmokeTest.kt
@@ -0,0 +1,11 @@
+package de.fgna.pocketdev
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PocketDevSmokeTest {
+    @Test
+    fun sanityCheck() {
+        assertEquals(4, 2 + 2)
+    }
+}
