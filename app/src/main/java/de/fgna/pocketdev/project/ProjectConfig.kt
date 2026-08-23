58765ec65eb4a775e61e4a1eb077f9eb26d52444
fgna <50234103+fgna@users.noreply.github.com>
2026-08-23 19:40:06 +0200

    Add Sprint 2 project model and command builder

---
+++ app/src/main/java/de/fgna/pocketdev/project/ProjectConfig.kt
@@ -0,0 +1,28 @@
+package de.fgna.pocketdev.project
+
+data class ProjectConfig(
+    val name: String,
+    val remotePath: String,
+    val testCommand: String,
+    val buildCommand: String,
+)
+
+enum class ProjectAction {
+    GIT_STATUS,
+    TEST,
+    BUILD,
+}
+
+object ProjectCommandBuilder {
+    fun command(project: ProjectConfig, action: ProjectAction): String {
+        val actionCommand = when (action) {
+            ProjectAction.GIT_STATUS -> "git status --short --branch"
+            ProjectAction.TEST -> project.testCommand.trim()
+            ProjectAction.BUILD -> project.buildCommand.trim()
+        }
+        require(actionCommand.isNotBlank()) { "Project action command is blank." }
+        return "cd ${shellQuote(project.remotePath)} && $actionCommand"
+    }
+
+    fun shellQuote(value: String): String = "'" + value.replace("'", "'\"'\"'") + "'"
+}
