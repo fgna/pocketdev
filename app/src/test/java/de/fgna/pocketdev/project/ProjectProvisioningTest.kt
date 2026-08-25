dcd741e00d7e8b20d6deec2a9226b1ccab81c8ce
fgna <50234103+fgna@users.noreply.github.com>
2026-08-25 06:27:26 +0200

    Test bootstrap verifies created directory

--- app/src/test/java/de/fgna/pocketdev/project/ProjectProvisioningTest.kt
+++ app/src/test/java/de/fgna/pocketdev/project/ProjectProvisioningTest.kt
@@ -4,34 +4,37 @@ import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class ProjectProvisioningTest {
-    private fun project(repo: String = "fgna/example") = ProjectConfig(
+    private val project = ProjectConfig(
         id = "p1",
-        name = "Example",
-        remotePath = "/home/freya/Projects/example",
+        name = "PocketDev test",
+        remotePath = "/home/freya/Projects/new project",
         testCommand = "./gradlew test",
         buildCommand = "./gradlew assembleDebug",
-        githubRepository = repo,
+        githubRepository = "fgna/pocketdev",
     )
 
     @Test
-    fun checkCommandReportsMissingAndExistingMarkers() {
-        val command = ProjectProvisioning.checkCommand(project())
-        assertTrue(command.contains("[ -d '/home/freya/Projects/example' ]"))
+    fun checkCommandReportsExistsOrMissing() {
+        val command = ProjectProvisioning.checkCommand(project)
         assertTrue(command.contains(ProjectProvisioning.EXISTS_MARKER))
         assertTrue(command.contains(ProjectProvisioning.MISSING_MARKER))
+        assertTrue(command.contains("'/home/freya/Projects/new project'"))
     }
 
     @Test
-    fun createCommandClonesConfiguredGithubRepository() {
-        val command = ProjectProvisioning.createCommand(project())
-        assertTrue(command.contains("git clone 'git@github.com:fgna/example.git' '/home/freya/Projects/example'"))
+    fun createCommandClonesConfiguredRepositoryAndVerifiesFolder() {
+        val command = ProjectProvisioning.createCommand(project)
+        assertTrue(command.contains("git clone 'git@github.com:fgna/pocketdev.git' '/home/freya/Projects/new project'"))
+        assertTrue(command.contains("mkdir -p '/home/freya/Projects'"))
+        assertTrue(command.contains("test -d '/home/freya/Projects/new project'"))
         assertTrue(command.contains(ProjectProvisioning.CREATED_MARKER))
     }
 
     @Test
-    fun createCommandOnlyMakesDirectoryWithoutRepository() {
-        val command = ProjectProvisioning.createCommand(project(repo = ""))
-        assertTrue(command.contains("mkdir -p '/home/freya/Projects/example'"))
-        assertTrue(!command.contains("git clone"))
+    fun createCommandMakesEmptyFolderWhenRepositoryIsBlank() {
+        val command = ProjectProvisioning.createCommand(project.copy(githubRepository = ""))
+        assertTrue(command.contains("mkdir -p '/home/freya/Projects/new project'"))
+        assertTrue(command.contains("test -d '/home/freya/Projects/new project'"))
+        assertTrue(command.contains(ProjectProvisioning.CREATED_MARKER))
     }
 }
