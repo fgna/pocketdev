62fa632f8c2aa1f990cb99a73717b9aeb50ed632
fgna <50234103+fgna@users.noreply.github.com>
2026-08-30 21:48:14 +0200

    Prepare PocketDev 1.0.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.fgna.pocketdev"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
